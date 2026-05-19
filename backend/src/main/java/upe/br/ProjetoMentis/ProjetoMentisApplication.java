f55bf800e9d45889f09104183c3cb4f3eb8408fb
Murilo Lucena <muriloglucenacanal@gmail.com>
2026-05-19 18:26:22 -0300

    feat: versão incial do backend

---
+++ backend/src/main/java/upe/br/ProjetoMentis/ProjetoMentisApplication.java
@@ -0,0 +1,13 @@
+package upe.br.ProjetoMentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMentisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMentisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/upe/br/ProjetoMentis/ProjetoMentisApplicationTests.java
@@ -0,0 +1,13 @@
+package upe.br.ProjetoMentis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMentisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
