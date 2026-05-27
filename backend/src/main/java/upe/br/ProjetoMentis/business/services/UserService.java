a9319bc8042d49232fe2710aed7a0c7289a96ee6
Pedro-RGS <pedro.gsouza@upe.br>
2026-05-26 22:19:43 -0300

    feat: inject dependencies in UserService class

--- backend/src/main/java/upe/br/ProjetoMentis/business/services/UserService.java
+++ backend/src/main/java/upe/br/ProjetoMentis/business/services/UserService.java
@@ -1,4 +1,13 @@
 package upe.br.ProjetoMentis.business.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import upe.br.ProjetoMentis.infra.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
 }
