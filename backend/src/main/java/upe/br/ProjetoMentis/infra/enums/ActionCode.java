a8f0a86f67e79ff4cc5483a8617e104e715f2e4a
Murilo Lucena <muriloglucenacanal@gmail.com>
2026-06-09 20:08:45 -0300

    refactor: Códigos de Ação reescritos

--- backend/src/main/java/upe/br/ProjetoMentis/infra/enums/ActionCode.java
+++ backend/src/main/java/upe/br/ProjetoMentis/infra/enums/ActionCode.java
@@ -1,40 +1,45 @@
 package upe.br.ProjetoMentis.infra.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ActionCode {
-    C0301080208 ("Atendimento Individual"),
 
-    C0301080216 ("Atendimento em Grupo"),
+    C0301080208("03.01.08.020-8", "Atendimento Individual"),
 
-    // Família
-    C0301080224 ("Atendimento em Família"),
-    C0301080259 ("Atendimento à Família"),
+    C0301080216("03.01.08.021-6", "Atendimento em Grupo"),
 
-    C0301080232 ("Visita Domiciliar"),
+    C0301080224("03.01.08.022-4", "Atendimento em Família"),
 
-    C0301080267 ("Acolhimento Inicial com Classificação de Risco"),
+    C0301080232("03.01.08.023-2", "Visita Domiciliar"),
 
-    C0301080275 ("Elaboração de Projeto Terapêutico Singular"),
+    C0301080240("03.01.08.024-0", "Atividade Comunitária e/ou Grupal"),
 
-    C0301080283 ("Atendimento de Urgência/Crise CAPS"),
+    C0301080259("03.01.08.025-9", "Atendimento à Família"),
 
-    C0301080291 ("Escuta Qualificada e Orientação"),
+    C0301080267("03.01.08.026-7", "Acolhimento Inicial com Classificação de Risco"),
 
-    C0301080305 ("Oficina Terapêutica I (Cognição)"),
+    C0301080275("03.01.08.027-5", "Elaboração de Projeto Terapêutico Singular"),
 
-    C0301080313 ("Oficina Terapêutica II (Expressão)"),
+    C0301080283("03.01.08.028-3", "Atendimento de Urgência/Crise CAPS"),
 
-    // Redução de Danos
-    C0301080321 ("Redução de Danos - Individual"),
-    C0301080330 ("Redução de Danos - Grupal"),
+    C0301080291("03.01.08.029-1", "Escuta Qualificada e Orientação"),
 
-    C0301080240 ("Atividade Comunitária e/ou Grupal"),
+    C0301080305("03.01.08.030-5", "Oficina Terapêutica I (Cognição)"),
 
-    C0301080348 ("Coordenação de Cuidados - Matriciamento");
+    C0301080313("03.01.08.031-3", "Oficina Terapêutica II (Expressão)"),
 
+    C0301080321("03.01.08.032-1", "Redução de Danos - Individual"),
 
+    C0301080330("03.01.08.033-0", "Redução de Danos - Grupal"),
+
+    C0301080348("03.01.08.034-8", "Coordenação de Cuidados - Matriciamento");
+
+    private final String code;
     private final String name;
 
-    private ActionCode(final String name) {
+    ActionCode(String code, String name) {
+        this.code = code;
         this.name = name;
     }
 }
