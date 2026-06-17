7c0f9dc2e635c86c879054f1d06de6bf0d66a8d2
Pedro-RGS <pedro.gsouza@upe.br>
2026-06-16 22:00:28 -0300

    feat: add login endpoint

--- backend/src/main/java/upe/br/ProjetoMentis/business/services/user/UserService.java
+++ backend/src/main/java/upe/br/ProjetoMentis/business/services/user/UserService.java
@@ -1,6 +1,7 @@
 package upe.br.ProjetoMentis.business.services.user;
 
 import upe.br.ProjetoMentis.controller.dtos.user.CreateUserDto;
+import upe.br.ProjetoMentis.controller.dtos.user.LoginDto;
 import upe.br.ProjetoMentis.controller.dtos.user.UpdateUserDto;
 import upe.br.ProjetoMentis.controller.dtos.user.UserResponseDto;
 
@@ -14,5 +15,6 @@ public interface UserService {
     UserResponseDto updateUser(UUID id, UpdateUserDto user);
     void validateEmailUniqueness(String email);
     void validateEmailForUpdate(String newEmail, UUID currentUserId);
+    void login(LoginDto loginDto);
     void deleteUser(UUID id);
 }
